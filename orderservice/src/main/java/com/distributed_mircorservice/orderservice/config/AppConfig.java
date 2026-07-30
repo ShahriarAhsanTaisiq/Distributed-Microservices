b8e7509cb2ade6d98c0f0793f2b2128a5a8c8ae6
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2026-07-31 00:38:22 +0600

    Commented RestTemplate manual setTimeout communication approach.

--- orderservice/src/main/java/com/distributed_mircorservice/orderservice/config/AppConfig.java
+++ orderservice/src/main/java/com/distributed_mircorservice/orderservice/config/AppConfig.java
@@ -13,19 +13,18 @@ public class AppConfig {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
-
-    // Another way using RestTemplate if we want to set timeouts manually
-
-    @Bean
-    public RestTemplate getRestTemplate2() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-
-        // Set timeout in milliseconds
-        factory.setConnectTimeout(1000); // 1 second for connection timeout
-        factory.setReadTimeout(5000); // 5 seconds for response timeout
-
-        return new RestTemplate();
-    }
-
+//
+//    // Another way using RestTemplate if we want to set timeouts manually
+//
+//    @Bean
+//    public RestTemplate getRestTemplate2() {
+//        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+//
+//        // Set timeout in milliseconds
+//        factory.setConnectTimeout(1000); // 1 second for connection timeout
+//        factory.setReadTimeout(5000); // 5 seconds for response timeout
+//
+//        return new RestTemplate(factory);
+//    }
 
 }
