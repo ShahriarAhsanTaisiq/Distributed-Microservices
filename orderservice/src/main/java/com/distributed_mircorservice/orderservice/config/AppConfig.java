d72f0667c9cc2fb1e4fac5671651b209ad787190
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2026-07-30 23:49:10 +0600

    Added app configuration using RestTemplate with manual setTimeout approach.

--- orderservice/src/main/java/com/distributed_mircorservice/orderservice/config/AppConfig.java
+++ orderservice/src/main/java/com/distributed_mircorservice/orderservice/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.distributed_mircorservice.orderservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,5 +13,19 @@ public class AppConfig {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    // Another way using RestTemplate if we want to set timeouts manually
+
+    @Bean
+    public RestTemplate getRestTemplate2() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        // Set timeout in milliseconds
+        factory.setConnectTimeout(1000); // 1 second for connection timeout
+        factory.setReadTimeout(5000); // 5 seconds for response timeout
+
+        return new RestTemplate();
+    }
+
 
 }
