7177bc4e76956ea56454d260e9681a6fd009dcad
Shahriar AhsanTaisiq <shahriar.ahsan@sonaliintellect.com>
2026-08-02 16:39:04 +0600

    API Requestion with RestClient.

--- orderservice/src/main/java/com/distributed_mircorservice/orderservice/service/OrderServiceImpl.java
+++ orderservice/src/main/java/com/distributed_mircorservice/orderservice/service/OrderServiceImpl.java
@@ -2,26 +2,36 @@ package com.distributed_mircorservice.orderservice.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     @Value("${product.service.base-url}")
     private String baseUrl;
 
-    public OrderServiceImpl(RestTemplate restTemplate) {
+    public OrderServiceImpl(RestTemplate restTemplate, RestClient restClient) {
         this.restTemplate = restTemplate;
+        this.restClient = restClient;
     }
 
     // RestTemplate Synchronous Communication
     @Override
     public String getOrderDtl(Integer id) {
 
-        // invoke product API
-        String response = restTemplate.getForObject(baseUrl+ "/products/{id}", String.class, id); // RestTemplate
+        // invoke product API through RestTemple Approach
+//        String response = restTemplate.getForObject(baseUrl+ "/products/{id}", String.class, id); // RestTemplate
+
+        // RestClient request
+        String response =  restClient
+                .get()
+                        .uri(baseUrl+ "/products/{id}", id)
+                                .retrieve()
+                                        .body(String.class);
         System.out.println("Response from Product Service call from Order Service: " + response);
         return response;
     }
