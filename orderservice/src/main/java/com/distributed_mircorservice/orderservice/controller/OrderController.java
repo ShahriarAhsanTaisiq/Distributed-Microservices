9db426a302a517ad5dfceb55934aefa120651839
Shahriar AhsanTaisiq <shahriar.ahsan@sonaliintellect.com>
2026-08-02 11:01:18 +0600

    Added Java based Http connection and API Request

--- orderservice/src/main/java/com/distributed_mircorservice/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/com/distributed_mircorservice/orderservice/controller/OrderController.java
@@ -23,4 +23,44 @@ public class OrderController {
         String response = orderService.getOrderDtl(id);
         return ResponseEntity.ok("Order call successful. " + response);
     }
+
+    /****** Only Java based Http connection and API Request *******/
+//        HttpURLConnection httpURLConnection = null;
+//        try{
+//            String url = "http://localhost:8084/products/" + id;
+//            URL obj = new URL(url);
+//
+//            /* Create an object of HttpURLConnection,
+//             consider it like an envelope or request in which specify all the details like
+//              URL, Request Methods and time etc. */
+//            httpURLConnection = (HttpURLConnection) obj.openConnection();
+//
+//            // Set HTTP request Method and Headers
+//            httpURLConnection.setRequestMethod("GET");
+//            httpURLConnection.setRequestProperty("Accept", "application/json");
+//
+//            // Set time to established TCP connection, timeout in millisecond
+//            httpURLConnection.setConnectTimeout(100);
+//            httpURLConnection.setReadTimeout(10000);
+//
+//            // Opens TCP Connection for triggered the http request and read response.
+//            BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+//            StringBuilder response = new StringBuilder();
+//            String responseLine;
+//            while ((responseLine = br.readLine()) != null){
+//                response.append(responseLine);
+//            }
+//            br.close();
+//            System.out.println("Response from server: " + response.toString());
+//
+//    } catch (Exception e){
+//        e.printStackTrace();
+//        } finally {
+//            if (httpURLConnection != null) {
+//                httpURLConnection.disconnect();
+//            }
+//        }
+//        return ResponseEntity.ok("Order call is successful. Response: " + response);
+//}
+
 }
