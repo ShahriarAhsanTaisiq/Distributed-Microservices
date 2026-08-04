437efb8e8c009b998ab5d03492f41887d1548f17
Shahriar AhsanTaisiq <shahriar.ahsan@sonaliintellect.com>
2026-08-04 14:57:30 +0600

    ..

--- orderservice/src/main/java/com/distributed_mircorservice/orderservice/controller/ProductClient.java
+++ orderservice/src/main/java/com/distributed_mircorservice/orderservice/controller/ProductClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 url = "${product.service.base-url}")
 public interface ProductClient {
     @GetMapping("/products/{id}")
-    public String  getProductById(@PathVariable Integer id);
+    String  getProductById(@PathVariable Integer id);
 }
